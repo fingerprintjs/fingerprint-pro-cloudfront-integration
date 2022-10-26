eb3c84629768ef5e8e14766a541f1456ca8cf6dd
Sergey Shelomentsev <sergey.shelomentsev@fingerprintjs.com>
2022-10-26 23:18:01 +0400

    rename

--- src/utils/traffic.ts
+++ src/utils/traffic.ts
@@ -2,13 +2,13 @@ const LAMBDA_FUNC_VERSION = '__lambda_func_version__'
 const PARAM_NAME = 'ii'
 
 export function addTrafficMonitoringSearchParamsForProCDN(url: URL) {
-  url.searchParams.append(PARAM_NAME, getHeaderValue('procdn'))
+  url.searchParams.append(PARAM_NAME, getTrafficMonitoringValue('procdn'))
 }
 
 export function addTrafficMonitoringSearchParamsForVisitorIdRequest(url: URL) {
-  url.searchParams.append(PARAM_NAME, getHeaderValue('ingress'))
+  url.searchParams.append(PARAM_NAME, getTrafficMonitoringValue('ingress'))
 }
 
-function getHeaderValue(type: 'procdn' | 'ingress'): string {
+function getTrafficMonitoringValue(type: 'procdn' | 'ingress'): string {
   return `fingerprintjs-pro-cloudfront/${LAMBDA_FUNC_VERSION}/${type}`
 }
