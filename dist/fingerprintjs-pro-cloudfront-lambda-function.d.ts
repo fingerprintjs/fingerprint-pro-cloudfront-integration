eff31af29cb9a7b96e602f9afca5fc88736388b0
Sergey Shelomentsev <sergey.shelomentsev@fingerprintjs.com>
2022-11-08 13:29:20 +0400

    dist

---
+++ dist/fingerprintjs-pro-cloudfront-lambda-function.d.ts
@@ -0,0 +1,10 @@
+/**
+ * FingerprintJS Pro Cloudfront Lambda function v0.0.1 - Copyright (c) FingerprintJS, Inc, 2022 (https://fingerprint.com)
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+ */
+
+import { CloudFrontRequestEvent, CloudFrontResultResponse } from 'aws-lambda';
+
+declare const handler: (event: CloudFrontRequestEvent) => Promise<CloudFrontResultResponse>;
+
+export { handler };
