a1b34c9db1cc0a1aee34d8b252d7788ebb629711
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-11-09 14:11:04 +0300

    feat: support s3 origin

--- test/utils/headers.test.ts
+++ test/utils/headers.test.ts
@@ -69,7 +69,7 @@ describe('test fpjs-headers preparation', () => {
 })
 
 describe('test custom headers', () => {
-  test('posivite scenario', () => {
+  test('positive scenario for custom origin', () => {
     const req: CloudFrontRequest = {
       clientIp: '1.1.1.1',
       method: 'GET',
@@ -85,6 +85,54 @@ describe('test custom headers', () => {
           protocol: 'https',
           readTimeout: 60,
           sslProtocols: ['TLSv2'],
+          customHeaders: {
+            fpjs_pre_shared_secret: [
+              {
+                key: 'fpjs_pre_shared_secret',
+                value: 'qwertyuio1356767',
+              },
+            ],
+            fpjs_agent_download_path: [
+              {
+                key: 'fpjs_agent_download_path',
+                value: 'greiodsfkljlds',
+              },
+            ],
+            fpjs_behavior_path: [
+              {
+                key: 'fpjs_behavior_path',
+                value: 'eifjdsnmzxcn',
+              },
+            ],
+            fpjs_get_result_path: [
+              {
+                key: 'fpjs_get_result_path',
+                value: 'eiwflsdkadlsjdsa',
+              },
+            ],
+          },
+        },
+      },
+    }
+
+    expect(getAgentUri(req)).toBe('/eifjdsnmzxcn/greiodsfkljlds')
+    expect(getResultUri(req)).toBe('/eifjdsnmzxcn/eiwflsdkadlsjdsa')
+    expect(getStatusUri(req)).toBe('/eifjdsnmzxcn/status')
+  })
+
+  test('positive scenario for s3 origin', () => {
+    const req: CloudFrontRequest = {
+      clientIp: '1.1.1.1',
+      method: 'GET',
+      uri: 'fpjs/agent',
+      querystring: 'apiKey=ujKG34hUYKLJKJ1F&version=3&loaderVersion=3.6.2',
+      headers: {},
+      origin: {
+        s3: {
+          domainName: 'adewe.cloudfront.net',
+          path: '/',
+          region: 'us',
+          authMethod: 'none',
           customHeaders: {
             fpjs_pre_shared_secret: [
               {
