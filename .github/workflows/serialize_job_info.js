2024945bd766ce32f743fdbac3bba47879c61c84
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-12-05 11:33:55 +0200

    chore: move meta.json to e2e directory

--- .github/workflows/serialize_job_info.js
+++ .github/workflows/serialize_job_info.js
@@ -4,9 +4,9 @@ const serializedData = JSON.stringify({
     sha: process.env.GITHUB_REAL_COMMIT_SHA
 }, null, ' ')
 
-fs.writeFile('./dist/meta.json', serializedData, (err) => {
-    if (err) {
-        console.error(err);
-    }
-    // file written successfully
-});
+fs.writeFile('./e2e/meta.json', serializedData, (err) => {
+  if (err) {
+    console.error(err)
+  }
+  // file written successfully
+})
