5fd14a90b666269884902184ec3f133e7c6a8504
Sergey Shelomentsev <sergey.shelomentsev@fingerprintjs.com>
2022-12-07 00:54:40 +0700

    lambda function to upgrade cloudfront distribution

---
+++ mgmt-lambda/rollup.config.js
@@ -0,0 +1,58 @@
+import typescript from '@rollup/plugin-typescript'
+import jsonPlugin from '@rollup/plugin-json'
+import external from 'rollup-plugin-peer-deps-external'
+import licensePlugin from 'rollup-plugin-license'
+import dtsPlugin from 'rollup-plugin-dts'
+import replace from '@rollup/plugin-replace'
+import { join } from 'path'
+const dotenv = require('dotenv')
+dotenv.config()
+
+const { dependencies = {} } = require('./package.json')
+const packageJson = require('./package.json')
+
+const inputFile = 'src/app.ts'
+const outputDirectory = 'dist'
+const artifactName = 'fingerprintjs-pro-cloudfront-lambda-function'
+
+const commonInput = {
+  input: inputFile,
+  plugins: [
+    jsonPlugin(),
+    typescript(),
+    external(),
+    replace({
+      __FPCDN__: process.env.FPCDN,
+      __INGRESS_API__: process.env.INGRESS_API,
+      __lambda_func_version__: packageJson.version,
+      preventAssignment: true,
+    }),  
+  ],
+}
+
+const commonOutput = {
+  name: 'fingerprintjs-pro-cloudfront-lambda-function',
+  exports: 'named',
+}
+
+export default [
+  {
+    ...commonInput,
+    external: Object.keys(dependencies),
+    output: [
+      {
+        ...commonOutput,
+        file: `${outputDirectory}/${artifactName}.js`,
+        format: 'cjs',
+      },
+    ],
+  },
+  {
+    ...commonInput,
+    plugins: [dtsPlugin()],
+    output: {
+      file: `${outputDirectory}/${artifactName}.d.ts`,
+      format: 'es',
+    },
+  },
+]
