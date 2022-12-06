c099db57a9ca335f374a8ea9c676e6cc792918ae
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-12-06 12:36:11 +0200

    chore: add cleanup lambdas script

--- e2e/infra/utils/resource.ts
+++ e2e/infra/utils/resource.ts
@@ -1,5 +1,3 @@
 export function resource(name: string) {
-  const prefix = process.env.COMMIT_SHA ?? 'e2e'
-
-  return `fpjs-dev-${prefix}-cloudfront-${name}`
+  return `fpjs-dev-e2e-cloudfront-${name}`
 }
