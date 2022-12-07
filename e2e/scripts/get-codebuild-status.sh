86a87293f86a474467c8c350eb609330207913d2
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-12-07 13:19:23 +0200

    ci: add more debug info, fix invalid status

--- e2e/scripts/get-codebuild-status.sh
+++ e2e/scripts/get-codebuild-status.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
+# Check if $CODEBUILD_BUILD_SUCCEEDING equals 1. If yes, then echo "success", otherwise echo "error"
 if [ "$CODEBUILD_BUILD_SUCCEEDING" == 1 ]; then
   echo "success"
 else
-  echo "failure"
+  echo "error"
 fi
