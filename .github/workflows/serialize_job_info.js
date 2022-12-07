3e4e094728c9c1e86fe29745816b6b694217527c
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2022-12-07 13:02:27 +0100

    chore: use real commit sha instead of pr merge commit sha

--- .github/workflows/serialize_job_info.js
+++ .github/workflows/serialize_job_info.js
@@ -1,20 +1,7 @@
 const fs = require('fs')
 
-// console.log(Object.keys(process.env).filter(key => key.includes('GITHUB')));
-// console.log(process.env.GITHUB_REPOSITORY_OWNER);
-// console.log(process.env.GITHUB_REPOSITORY);
-// console.log(process.env.GITHUB_JOB);
-// console.log(process.env.GITHUB_SERVER_URL);
-// console.log(process.env.GITHUB_RUN_ID);
-// console.log(process.env.GITHUB_BASE_REF);
-// console.log(process.env.GITHUB_HEAD_REF);
-// console.log(process.env.GITHUB_WORKSPACE);
-// console.log(process.env.GITHUB_SHA);
-//
-
-// process.exit(0);
 const serializedData = JSON.stringify({
-    sha: process.env.GITHUB_SHA
+    sha: process.env.GITHUB_REAL_COMMIT_SHA
 }, null, ' ')
 
 fs.writeFile('./dist/meta.json', serializedData, (err) => {
