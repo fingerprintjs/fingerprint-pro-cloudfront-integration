b98a9d92daa5e7072274385c49b8e94d23357975
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-12-07 13:32:50 +0200

    chore: remove script header

--- e2e/scripts/get-codebuild-status.sh
+++ e2e/scripts/get-codebuild-status.sh
@@ -1,6 +1,3 @@
-#!/usr/bin/env sh
-
-# Check if $CODEBUILD_BUILD_SUCCEEDING equals 1. If yes, then echo "success", otherwise echo "error"
 if [ "$CODEBUILD_BUILD_SUCCEEDING" == 1 ]; then
   echo "success"
 else
