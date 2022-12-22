f338c51cd19f5883151c3239cc741d58b6a0c2c9
Sergey Shelomentsev <sergey.shelomentsev@fingerprintjs.com>
2022-12-23 01:02:30 +0700

    update cookies func

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,20 +1,23 @@
-export function updateCookie(cookieValue: string, domainName: string): string {
-  const parts = cookieValue.split(';')
-
-  const updated = parts.map((it) => {
-    const s = it.trim()
-    const kv = s.split('=')
-    if (kv.length === 1) {
-      return s
-    } else {
-      if (kv[0].toLowerCase() === 'domain') {
-        kv[1] = domainName
-        return `${kv[0]}=${kv[1]}`
+export function adjustCookies(cookies: string[], domainName: string): string {
+  const newCookies: string[] = []
+  cookies.forEach((it) => {
+    const parts: string[] = it.split(';')
+    
+    parts.map((v: string) => {
+      const s = v.trim()
+      const kv = s.split('=')
+      if (kv.length === 1) {
+        newCookies.push(s)
       } else {
-        return s
+        if (kv[0].toLowerCase() === 'domain') {
+          kv[1] = domainName
+          newCookies.push(`${kv[0]}=${kv[1]}`)
+        } else {
+          newCookies.push(s)
+        }
       }
-    }
+    })
   })
 
-  return updated.join('; ').trim()
+  return newCookies.join('; ').trim()
 }
