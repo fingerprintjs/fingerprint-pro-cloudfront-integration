ab929f0322efae392763e6d68ceb83feda810abc
TheUnderScorer <TheUnderScorer@users.noreply.github.com>
2023-07-10 09:30:42 +0000

    Deploy prepared in [2023-07-10-09-30-07]

---
+++ 2023-07-10-09-30-07/tests/src/tests/visitorId.test.ts
@@ -0,0 +1,79 @@
+import { expect, Page } from '@playwright/test'
+import { waitForCloudfront } from '../cloudfront'
+import { isRequestIdValid } from '../utils/areVisitorIdAndRequestIdValid'
+import { cloudfrontTest } from '../cloudfrontTest'
+import { trackRequests } from '../utils/playwright'
+
+async function checkResponse(page: Page) {
+  const response = await page.waitForSelector('#response pre').then((element) => element.textContent())
+
+  expect(response).toBeTruthy()
+
+  const json = JSON.parse(response as string)
+
+  expect(isRequestIdValid(json.requestId)).toBeTruthy()
+}
+
+cloudfrontTest.describe('visitorId', () => {
+  cloudfrontTest.beforeEach(async () => {
+    await waitForCloudfront()
+  })
+
+  cloudfrontTest('should return 404 if customer variables are missing', async ({ page, urlType }) => {
+    if (urlType !== 'cloudfrontWithoutVariables') {
+      cloudfrontTest.skip()
+    }
+
+    const { getRequests } = trackRequests(page)
+
+    await page.goto('/', {
+      waitUntil: 'networkidle',
+    })
+
+    await page.click('#getData')
+
+    await page.waitForResponse(/fpjs\/visitorId/)
+
+    const requests = getRequests()
+    expect(requests).toHaveLength(5)
+
+    const [, , agentRequest, , apiRequest] = requests
+
+    const agentResponse = await agentRequest.response()
+    const apiRequestResponse = await apiRequest.response()
+
+    expect(agentResponse?.status()).toBe(200)
+    expect(apiRequestResponse?.status()).toBe(404)
+  })
+
+  cloudfrontTest('should show correct visitorId using lambda endpoints', async ({ page, urlType, baseURL }) => {
+    if (urlType === 'cloudfrontWithoutVariables') {
+      cloudfrontTest.skip()
+    }
+
+    const rootUrl = new URL(baseURL as string)
+
+    const { getRequests } = trackRequests(page)
+
+    await page.goto('/', {
+      waitUntil: 'networkidle',
+    })
+
+    await page.click('#getData')
+
+    await checkResponse(page)
+
+    const requests = getRequests()
+    expect(requests).toHaveLength(5)
+
+    const [, , agentRequest, , apiRequest] = requests
+
+    const agentRequestUrl = new URL(agentRequest.url())
+    expect(agentRequestUrl.hostname).toBe(rootUrl.hostname)
+
+    const apiRequestUrl = new URL(apiRequest.url())
+    expect(apiRequestUrl.hostname).toBe(rootUrl.hostname)
+    expect(apiRequestUrl.searchParams.get('ii')).toContain(`fingerprintjs-pro-cloudfront/`)
+    expect(apiRequestUrl.searchParams.get('ii')).toContain(`/procdn`)
+  })
+})
