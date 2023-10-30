f8d982c090debf9d7a14c722a7e4fe25e63c8582
TheUnderScorer <TheUnderScorer@users.noreply.github.com>
2023-10-30 14:24:13 +0000

    Deploy prepared in [2023-10-30-14-23-00]

---
+++ 2023-10-30-14-23-00/dist/fingerprintjs-pro-cloudfront-mgmt-lambda-function.d.ts
@@ -0,0 +1,8 @@
+/**
+ * FingerprintJS Pro CloudFront Lambda function v1.2.0 - Copyright (c) FingerprintJS, Inc, 2023 (https://fingerprint.com)
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+ */
+
+declare function handler(event: any, ctx: any): Promise<void>;
+
+export { handler };
