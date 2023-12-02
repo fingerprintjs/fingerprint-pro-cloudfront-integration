69d41c2707ef52c48dbe0a471a4942324b42efad
sshelomentsev <sshelomentsev@users.noreply.github.com>
2023-12-02 00:04:29 +0000

    Deploy prepared in [2023-12-02-00-03-38]

---
+++ 2023-12-02-00-03-38/dist/fingerprintjs-pro-cloudfront-mgmt-lambda-function.d.ts
@@ -0,0 +1,8 @@
+/**
+ * FingerprintJS Pro CloudFront Lambda function v1.3.1 - Copyright (c) FingerprintJS, Inc, 2023 (https://fingerprint.com)
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+ */
+
+declare function handler(event: any, ctx: any): Promise<void>;
+
+export { handler };
