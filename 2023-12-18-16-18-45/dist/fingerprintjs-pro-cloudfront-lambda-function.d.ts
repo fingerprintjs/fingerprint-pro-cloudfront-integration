9788fbaeed2ef15b0fc75e9914950d737a43f829
ilfa <ilfa@users.noreply.github.com>
2023-12-18 16:19:33 +0000

    Deploy prepared in [2023-12-18-16-18-45]

---
+++ 2023-12-18-16-18-45/dist/fingerprintjs-pro-cloudfront-lambda-function.d.ts
@@ -0,0 +1,10 @@
+/**
+ * FingerprintJS Pro CloudFront Lambda function v1.4.1-rc.1 - Copyright (c) FingerprintJS, Inc, 2023 (https://fingerprint.com)
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+ */
+
+import { CloudFrontRequestEvent, CloudFrontResultResponse } from 'aws-lambda';
+
+declare const handler: (event: CloudFrontRequestEvent) => Promise<CloudFrontResultResponse>;
+
+export { handler };
