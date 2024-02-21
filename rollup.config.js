9bd8e261d032a5534b196fd03be0ecf991549b61
Sergey Shelomentsev <sergey.shelomentsev@fingerprint.com>
2024-02-21 02:23:23 +0200

    chore: fix compilation

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import typescript from '@rollup/plugin-typescript'
 import jsonPlugin from '@rollup/plugin-json'
-import external from 'rollup-plugin-peer-deps-external'
 import licensePlugin from 'rollup-plugin-license'
 import { dts } from 'rollup-plugin-dts'
 import replace from '@rollup/plugin-replace'
@@ -44,11 +43,10 @@ function makeConfig(entryFile, artifactName) {
    * */
   const commonInput = {
     input: entryFile,
-    external: ['aws-sdk', 'https', '@aws-sdk/client-lambda', '@aws-sdk/client-codepipeline', '@aws-sdk/client-cloudfront'],
+    external: ['aws-sdk', 'https', '@aws-sdk/client-cloudfront', '@aws-sdk/client-codepipeline', '@aws-sdk/client-lambda'],
     plugins: [
       jsonPlugin(),
       typescript(),
-      external(),
       nodeResolve({ preferBuiltins: false }),
       commonjs(),
       replace({
@@ -83,7 +81,11 @@ function makeConfig(entryFile, artifactName) {
     },
     {
       ...commonInput,
-      plugins: [dts(), commonBanner],
+      plugins: [dts({
+        compilerOptions: {
+          preserveSymlinks: false
+        }
+      }), commonBanner],
       output: {
         file: `${outputDirectory}/${artifactName}.d.ts`,
         format: 'es',
