4470c2d3a99d043fdc5760f29ab2500d0e0d7b71
Sergey Shelomentsev <sergey.shelomentsev@fingerprint.com>
2024-02-29 22:50:16 +0200

    chore: remove fetch dependency

--- e2e/tests/src/cloudfront.ts
+++ e2e/tests/src/cloudfront.ts
@@ -1,7 +1,6 @@
 import path from 'path'
 import { getStackOutput } from '../../infra/utils/getStackOutput'
 import { wait } from './utils/wait'
-import fetch from 'node-fetch'
 
 const stackPath = path.resolve(__dirname, '../../infra/cloudfront')
 
