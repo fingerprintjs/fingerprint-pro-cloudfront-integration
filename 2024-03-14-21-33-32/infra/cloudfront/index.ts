527ec326d8db204879b6a881743fb7cd85085436
sshelomentsev <sshelomentsev@users.noreply.github.com>
2024-03-14 21:34:01 +0000

    Deploy prepared in [2024-03-14-21-33-32]

---
+++ 2024-03-14-21-33-32/infra/cloudfront/index.ts
@@ -0,0 +1,6 @@
+import './src/s3'
+import './src/lambda'
+
+export { cloudfrontWithHeadersUrl } from './src/cloudfront-with-headers'
+export { cloudfrontWithSecretsUrl } from './src/cloudfront-with-secrets'
+export { cloudfrontWithoutVariables } from './src/cloudfront-without-variables'
