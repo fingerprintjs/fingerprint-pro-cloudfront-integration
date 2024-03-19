3c4663917468eb51cdc2912dc476116acfcb1784
sshelomentsev <sshelomentsev@users.noreply.github.com>
2024-03-19 14:45:36 +0000

    Deploy prepared in [2024-03-19-14-45-01]

---
+++ 2024-03-19-14-45-01/dist/fingerprintjs-pro-cloudfront-mgmt-lambda-function.d.ts
@@ -0,0 +1,10 @@
+/**
+ * FingerprintJS Pro CloudFront Lambda function v2.0.1-rc.2 - Copyright (c) FingerprintJS, Inc, 2024 (https://fingerprint.com)
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+ */
+
+import { APIGatewayProxyEventV2WithRequestContext, APIGatewayEventRequestContextV2, APIGatewayProxyResult } from 'aws-lambda';
+
+declare function handler(event: APIGatewayProxyEventV2WithRequestContext<APIGatewayEventRequestContextV2>): Promise<APIGatewayProxyResult>;
+
+export { handler };
