21d1fb9c5fe096bae72bd38830bef2e6921ee3c6
sshelomentsev <sshelomentsev@users.noreply.github.com>
2024-04-30 20:14:40 +0000

    Deploy prepared in [2024-04-30-20-14-10]

---
+++ 2024-04-30-20-14-10/infra/cloudfront/src/lambda.ts
@@ -0,0 +1,31 @@
+import * as aws from '@pulumi/aws'
+import { resource } from '../../utils/resource'
+
+export const lambdaCachePolicy = new aws.cloudfront.CachePolicy(resource('lambda-cache-policy'), {
+  minTtl: 0,
+  maxTtl: 180,
+  defaultTtl: 180,
+  parametersInCacheKeyAndForwardedToOrigin: {
+    headersConfig: {
+      headerBehavior: 'none',
+    },
+    cookiesConfig: {
+      cookieBehavior: 'none',
+    },
+    queryStringsConfig: {
+      queryStringBehavior: 'all',
+    },
+  },
+})
+
+export const lambdaOriginPolicy = new aws.cloudfront.OriginRequestPolicy(resource('lambda-origin-policy'), {
+  headersConfig: {
+    headerBehavior: 'allViewer',
+  },
+  cookiesConfig: {
+    cookieBehavior: 'all',
+  },
+  queryStringsConfig: {
+    queryStringBehavior: 'all',
+  },
+})
