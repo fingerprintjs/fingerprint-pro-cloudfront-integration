7df3aa7ce1fbf00719aa488a593bbec07bffae31
Sergey Shelomentsev <sergey.shelomentsev@fingerprint.com>
2024-05-01 00:16:00 +0300

    test: fix cache policy

--- e2e/infra/cloudfront/src/lambda.ts
+++ e2e/infra/cloudfront/src/lambda.ts
@@ -3,8 +3,8 @@ import { resource } from '../../utils/resource'
 
 export const lambdaCachePolicy = new aws.cloudfront.CachePolicy(resource('lambda-cache-policy'), {
   minTtl: 0,
-  maxTtl: 0,
-  defaultTtl: 0,
+  maxTtl: 180,
+  defaultTtl: 180,
   parametersInCacheKeyAndForwardedToOrigin: {
     headersConfig: {
       headerBehavior: 'none',
@@ -13,7 +13,10 @@ export const lambdaCachePolicy = new aws.cloudfront.CachePolicy(resource('lambda
       cookieBehavior: 'none',
     },
     queryStringsConfig: {
-      queryStringBehavior: 'none',
+      queryStringBehavior: 'allExcept',
+      queryStrings: {
+        items: ['version'],
+      },
     },
   },
 })
