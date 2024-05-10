a02fbf9595851dad67b52c7b6edfb5c5db0b5540
Orkun <orkuncakilkaya@gmail.com>
2024-05-10 10:48:30 +0300

    fix: set log level

--- proxy/utils/log.ts
+++ proxy/utils/log.ts
@@ -1,8 +1,13 @@
 import { CloudFrontRequest } from 'aws-lambda'
 import { getHeaderValue } from './headers'
 
+/**
+ * This function depends on 'fpjs_debug' boolean value, determining whether debug logs are going to be logged or not.
+ * @param request {CloudFrontRequest}
+ */
 export function setLogLevel(request?: CloudFrontRequest) {
   const debugValue = request ? getHeaderValue(request, 'fpjs_debug') : undefined
-  const level = debugValue === 'true' ? 'DEBUG' : 'ERROR'
-  process.env.AWS_LAMBDA_LOG_LEVEL = level
+  if (!debugValue) {
+    console.debug = () => null
+  }
 }
