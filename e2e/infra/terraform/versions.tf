2a52730754bdbe76c801f19e4bde94490cc44510
Diego Cotelo <diego.cotelo@fingerprint.com>
2025-04-24 11:29:27 -0300

    ci: initial CI setup

---
+++ e2e/infra/terraform/versions.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "~> 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.95.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.1"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.7.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
+    }
+  }
+}
