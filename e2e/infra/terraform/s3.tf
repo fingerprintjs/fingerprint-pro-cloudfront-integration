1fdbfb26b6308634cf94014bbb76827ca94e6d07
Diego Cotelo <diego.cotelo@fingerprint.com>
2025-04-25 14:34:10 -0300

    ci: test s3

--- e2e/infra/terraform/s3.tf
+++ e2e/infra/terraform/s3.tf
@@ -18,27 +18,6 @@ resource "aws_s3_bucket_website_configuration" "website_endpoint" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "website_bucket_access_block" {
-  bucket              = aws_s3_bucket.website_bucket.id
-  block_public_acls   = false
-  block_public_policy = false
-}
-
-resource "aws_s3_bucket_policy" "website_bucket_policy" {
-  bucket = aws_s3_bucket.website_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = ["s3:GetObject"],
-        Resource  = ["${aws_s3_bucket.website_bucket.arn}/*"]
-      }
-    ]
-  })
-}
 
 
 
