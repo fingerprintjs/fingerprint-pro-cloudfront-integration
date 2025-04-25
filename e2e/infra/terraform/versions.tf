851609f5d09fc23579b41682298765e4eae70ba4
Diego Cotelo <diego.cotelo@fingerprint.com>
2025-04-25 08:58:21 -0300

    ci: fix credentials

--- e2e/infra/terraform/versions.tf
+++ e2e/infra/terraform/versions.tf
@@ -5,5 +5,21 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.59.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.1"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.7.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
+    }
   }
 }
