d044339d13de493b5c6a3fd527c5732d371c2b6a
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-05-09 15:02:27 +0200

    chore: pass zip package

--- e2e/infra/terraform/fingerprint.tf
+++ e2e/infra/terraform/fingerprint.tf
@@ -5,5 +5,5 @@ module "fingerprint_cloudfront_integration" {
   fpjs_get_result_path     = var.fpjs_get_result_path
   fpjs_shared_secret       = var.fpjs_shared_secret
   fetch_lambda_from_s3     = false
-  local_lambda_path        = "../../../dist/fingerprintjs-pro-cloudfront-lambda-function.js"
+  local_lambda_path        = "../../../package.zip"
 }
