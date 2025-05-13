18a0cf1dc36e5c21ae60453312d7d1590b0312ba
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-05-13 12:17:38 +0200

    chore: remove pulumi

--- e2e/infra/cloudfront/index.ts
+++
@@ -1,6 +0,0 @@
-import './src/s3'
-import './src/lambda'
-
-export { cloudfrontWithHeadersUrl } from './src/cloudfront-with-headers'
-export { cloudfrontWithSecretsUrl } from './src/cloudfront-with-secrets'
-export { cloudfrontWithoutVariables } from './src/cloudfront-without-variables'
